c45ef1bdca2182ebc50b4947d38116ffaa1918ec
Shawn Tan <shawn@wtf.sg>
2014-10-30 02:19:37 +0800

    Params

--- utils.py
+++ utils.py
@@ -3,14 +3,21 @@ import theano
 theano.config.floatX='float32'
 from theano.tensor.shared_randomstreams import RandomStreams
 import random
+
+
 theano_rng = RandomStreams(np.random.RandomState(1234).randint(2**30))
 np.random.seed(1234)
 random.seed(1234)
-def initial_weights(*argv):
-	return 0.1 * np.random.randn(*argv)
-#	scale = np.sqrt(6./sum(argv))
-#	return (8 * scale) * np.random.rand(*argv) - (4 * scale)
 
+def initial_weights(*argv):
+	return np.asarray(
+		np.random.uniform(
+			low  = -np.sqrt(6. / sum(argv)),
+			high =  np.sqrt(6. / sum(argv)),
+			size =  argv
+		),
+		dtype=theano.config.floatX
+	)
 
 def create_shared(array, dtype=theano.config.floatX, name=None):
 	return theano.shared(
