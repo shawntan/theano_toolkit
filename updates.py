c25088199fbca70bb88074c99e9b2922fe6b9075
Shawn Tan <shawn@wtf.sg>
2015-08-19 15:28:29 +0800

    Parameters for momentum

--- updates.py
+++ updates.py
@@ -13,9 +13,10 @@ def adadelta(parameters,gradients,rho=np.float32(0.95),eps=np.float32(1e-4),P=Pa
     shapes = [ p.get_value().shape for p in parameters ]
     gradients_sq = [ create_param(P,"grad_sq_" + p.name,np.zeros(s))   for p,s in izip(parameters,shapes) ]
     deltas_sq    = [ create_param(P,"deltas_sq_" + p.name,np.zeros(s)) for p,s in izip(parameters,shapes) ]
-    gradients_sq_new = [ rho*g_sq + (np.float32(1)-rho)*(g**2) for g_sq,g         in izip(gradients_sq,gradients) ]
-    deltas = [ (T.sqrt(d_sq+eps)/T.sqrt(g_sq+eps))*grad        for d_sq,g_sq,grad in izip(deltas_sq,gradients_sq_new,gradients) ]
-    deltas_sq_new = [ rho*d_sq + (np.float32(1)-rho)*(d**2)    for d_sq,d         in izip(deltas_sq,deltas) ]
+    gradients_sq_new = [ rho*g_sq + (np.float32(1)-rho)*(g**2) for g_sq,g in izip(gradients_sq,gradients) ]
+
+    deltas = [ (T.sqrt(d_sq+eps)/T.sqrt(g_sq+eps))*grad for d_sq,g_sq,grad in izip(deltas_sq,gradients_sq_new,gradients) ]
+    deltas_sq_new = [ rho*d_sq + (np.float32(1)-rho)*(d**2) for d_sq,d in izip(deltas_sq,deltas) ]
 
     gradient_sq_updates = zip(gradients_sq,gradients_sq_new)
     deltas_sq_updates   = zip(deltas_sq,deltas_sq_new)
@@ -24,11 +25,12 @@ def adadelta(parameters,gradients,rho=np.float32(0.95),eps=np.float32(1e-4),P=Pa
     return gradient_sq_updates + deltas_sq_updates + parameters_updates
 
 
-def momentum(parameters,gradients,mu=0.9,eps=1e-3):
-    t = U.create_shared(1)
+def momentum(parameters,gradients,mu=0.9,eps=1e-3,P=Parameters()):
+    P.t = 1
     m = (1 - 3.0/(t+5) < mu)
     mu = m * (1 - 3.0/(t+5)) + (1-m) * mu
-    deltas = [ U.create_shared(np.zeros(p.get_value().shape)) for p in parameters ]
+    shapes = [ p.get_value().shape for p in parameters ]
+    deltas = [ create_param(P,"deltas_" + p.name,np.zeros(s)) for p,s in izip(parameters,shapes) ]
     delta_nexts = [ mu*delta + eps*grad for delta,grad in zip(deltas,gradients) ]
     delta_updates = [ (delta, delta_next) for delta,delta_next in zip(deltas,delta_nexts) ]
     param_updates = [ (param, param - delta_next) for param,delta_next in zip(parameters,delta_nexts) ]
